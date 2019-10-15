276bd6cf798b8ad4bb1615cf9efbe7c1129176bb
nnachevvv <nnachevv@icloud.com>
2019-10-15 17:02:10 +0300

    Refactoring code.

--- Board.js
+++ Board.js
@@ -1,15 +1,5 @@
 "use strict";
-let DEFAULT_ROWS = 8;
-let DEFAULT_COLS = 8;
 
-
-
-
-
-function bombUpdateHtml() {
-    document.getElementById('bombsLeft').innerText = game.bombsLeft + "";
-
-}
 
 
 
@@ -20,7 +10,6 @@ class Game {
         this.row = row;
         this.col = col;
         this.cellsToWin = row * col - bombs;
-        this.bombs = bombs;
         this.bombsLeft = bombs;
         this.isGameFinished = false;
         this.bombsPositions = new Set([]);
@@ -28,14 +17,42 @@ class Game {
         this.stopwatch = new StopWatch();
     };
 
+    startGame(ROWS =  8, COLS = 8, BOMBS = 10 ) {
+        document.getElementById("face").src = 'images/bored.gif';
+        game.stopwatch.stopInterval();
+        game = new Game(ROWS, COLS,  BOMBS);
+    }
+
     defineBoard() {
+        this.clearTable();
         this.board = new Array(this.row);
         for (let i = 0; i < this.col; i++) {
             this.board[i] = new Array(this.col);
         }
         this.setBombs();
-        this.describeBombs();
+        this.describeCells();
+        this.bombUpdateHtml();
     };
+
+
+    clearTable() {
+        for (let i = 0; i < this.row; i++) {
+            for (let j = 0; j < this.col; j++) {
+
+                document.getElementById("tableId").rows[i + 1].cells[j].innerText = " ";
+                document.getElementById("tableId").rows[i + 1].cells[j].style.backgroundColor = "gainsboro";
+                document.getElementById("tableId").rows[i + 1].cells[j].style.pointerEvents = 'auto';
+            }
+        }
+        let el = document.getElementById("timer");
+        el.innerText = 0 + " ";
+    };
+
+
+
+     bombUpdateHtml() {
+        document.getElementById('bombsLeft').innerText = this.bombsLeft + "";
+    }
 
     ifOccupied(pos) {
         return this.board[pos.row][pos.col];
@@ -46,19 +63,21 @@ class Game {
     }
 
     setBombs() {
-        for (let i = 0; i < this.bombs; i++) {
+        for (let i = 0; i < this.bombsLeft; i++) {
 
             let pos = generateRandomPosition(this.row, this.col);
 
             while (this.ifOccupied(pos)) {
                 pos = generateRandomPosition(this.row, this.col);
             }
+
             this.board[pos.row][pos.col] = "X";
             this.bombsPositions.add(pos.row + " " + pos.col);
+
         }
     };
 
-    checkTopBombs(i, j) {
+    countOfTopBombs(i, j) {
         let bombs = 0;
         if (i - 1 >= 0 && this.isBombPlanned(i - 1, j)) {
             ++bombs;
@@ -74,7 +93,7 @@ class Game {
         return bombs;
     };
 
-    checkBottomBombs(i, j) {
+    countOfBottomBombs(i, j) {
         let bombs = 0;
         if (i + 1 < this.row && this.isBombPlanned(i + 1, j)) {
             ++bombs;
@@ -88,9 +107,9 @@ class Game {
             ++bombs;
         }
         return bombs;
-    }
+    };
 
-    checkLeftRightBombs(i, j) {
+    CountOfLeftAndRightBombs(i, j) {
         let bombs = 0;
 
         if (j - 1 >= 0 && this.isBombPlanned(i, j - 1)) {
@@ -102,41 +121,40 @@ class Game {
         }
 
         return bombs;
-    }
+    };
 
-    describeBombs() {
+    describeCells() {
         for (let i = 0; i < this.row; i++) {
             for (let j = 0; j < this.col; j++) {
                 if (this.board[i][j] !== "X") {
-                    this.board[i][j] = this.checkTopBombs(i, j) + this.checkBottomBombs(i, j) + this.checkLeftRightBombs(i, j);
+                    this.board[i][j] = this.countOfTopBombs(i, j) + this.countOfBottomBombs(i, j) + this.CountOfLeftAndRightBombs(i, j);
                 }
             }
         }
-    }
+    };
 
     checkForWin() {
-
         if (this.cellsToWin <= 0) {
-            this.showBombs('!');
+            this.markBombs('!');
             document.getElementById("face").src = 'images/happy.gif';
             return true;
         }
-    }
+    };
 
     gameOverClick()
     {
         this.stopwatch.stopInterval();
-        this.showBombs();
+        this.markBombs();
         this.isGameFinished = true;
         document.getElementById("face").src = 'images/sad.gif';
-    }
+    };
 
     rightClickMouse(click)
     {
         if (click.innerText === '?') {
             click.innerText = '!';
             ++this.bombsLeft;
-            bombUpdateHtml();
+            this.bombUpdateHtml();
         }else if(click.innerText === '!')
         {
             click.innerText = null;
@@ -144,73 +162,65 @@ class Game {
         else {
             click.innerText = '?';
             --this.bombsLeft;
-            bombUpdateHtml();
+            this.bombUpdateHtml();
         }
-    }
+    };
 
-    showBlock(click, event) {
+    leftClickMouse(click)
+    {
+        click.style.pointerEvents = 'none';
+        let rowClicked = click.parentNode.rowIndex - 1;
+        let colClicked = click.cellIndex;
+        if (this.board[rowClicked][colClicked] === 0) {
+            this.clearZeroCells(rowClicked,colClicked);
+        }else if(this.board[rowClicked][colClicked] !== 'X'){
+            --this.cellsToWin;
+            click.innerText = this.board[rowClicked][colClicked];
+            click.style.backgroundColor = "rgb(169,169, 169)\n";
+        }
+        if (this.board[rowClicked][colClicked] === "X") {
+            this.gameOverClick();
+        } else if (!this.stopwatch.timerStarted) {
+            this.stopwatch.startTime();
+        }
+    };
+
+
+
+    showCell(click, event) {
         event.preventDefault();
         if (!this.isGameFinished) {
-            let rowClicked = click.parentNode.rowIndex - 1;
-            let colClicked = click.cellIndex;
+
             if (event.button === 0) {
-                click.style.pointerEvents = 'none';
-                if (this.board[rowClicked][colClicked] === 0) {
-                    this.clearZeroCells(rowClicked,colClicked);
-                }else{
-                    --this.cellsToWin;
-                    click.innerText = this.board[rowClicked][colClicked];
-                    click.style.backgroundColor = "rgb(169,169, 169)\n";
-                }
-                if (this.board[rowClicked][colClicked] === "X") {
-                    this.gameOverClick();
-                } else if (!this.stopwatch.timerStarted) {
-                    this.stopwatch.startTime();
-                }
+                this.leftClickMouse(click);
             } else {
                 this.rightClickMouse(click);
             }
         }
         if (this.checkForWin()) {
             this.isGameFinished = true;
+            this.bombsLeft = 0;
             this.stopwatch.stopInterval();
         }
-    }
+    };
 
-    showBombs(symbol = 'X') {
+    markBombs(symbol = 'X') {
         for (let bomb of this.bombsPositions.values()) {
             bomb = bomb.split(" ");
             let row = Number(bomb[0]);
             let col = Number(bomb[1]);
-            document.getElementById("tableId").rows[row + 1].cells[col].innerText = symbol;
+            let element = document.getElementById("tableId").rows[row + 1].cells[col];
+            element.innerText = symbol;
                 if(symbol === 'X')
             {
-                document.getElementById("tableId").rows[row + 1].cells[col].style.backgroundColor = "rgb(169,169, 169)\n";
+                element.style.backgroundColor = "rgb(169,169, 169)\n";
             }
 
         }
-    }
+    };
 
-    clearTable() {
-        for (let i = 0; i < this.row; i++) {
-            for (let j = 0; j < this.col; j++) {
 
-                document.getElementById("tableId").rows[i + 1].cells[j].innerText = " ";
-                document.getElementById("tableId").rows[i + 1].cells[j].style.backgroundColor = "gainsboro";
-                document.getElementById("tableId").rows[i + 1].cells[j].style.pointerEvents = 'auto';
-            }
-        }
-        let el = document.getElementById("timer");
-        el.innerText = 0 + " ";
-    }
 
-    startGame() {
-        document.getElementById("face").src = 'images/bored.gif';
-        game.stopwatch.stopInterval();
-        game = new Game(8, 8, 10);
-        bombUpdateHtml();
-        this.clearTable();
-    }
 
     IsTableCellEmpty(row , col)
     {
@@ -274,8 +284,7 @@ class Game {
 
 }
 
-let game = new Game(DEFAULT_ROWS, DEFAULT_COLS, 10);
-bombUpdateHtml();
+let game = new Game(8, 8, 10);
 
 
 
